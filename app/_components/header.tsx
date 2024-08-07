26be33c85fe34fc6a2f725fdafea537b6c48e4d7
beatrizalvesfel <beatrizalvesfel@gmail.com>
2024-08-07 19:44:26 -0300

    feat: add  SidebarSheet

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -2,8 +2,8 @@ import Image from "next/image"
 import { Card, CardContent } from "./ui/card"
 import { Button } from "./ui/button"
 import { MenuIcon } from "lucide-react"
-// import { Sheet, SheetTrigger } from "./ui/sheet"
-// import SidebarSheet from "./sidebar-sheet"
+import { Sheet, SheetTrigger } from "./ui/sheet"
+import SidebarSheet from "./sidebar-sheet"
 
 const Header = () => {
   return (
@@ -11,14 +11,14 @@ const Header = () => {
       <CardContent className="flex flex-row items-center justify-between p-5">
         <Image alt="FSW Barber" src="/logo.png" height={18} width={120} />
 
-        {/* <Sheet> */}
-        {/* <SheetTrigger asChild> */}
-        <Button size="icon" variant="outline">
-          <MenuIcon />
-        </Button>
-        {/* </SheetTrigger> */}
-        {/* <SidebarSheet /> */}
-        {/* </Sheet> */}
+        <Sheet>
+          <SheetTrigger asChild>
+            <Button size="icon" variant="outline">
+              <MenuIcon />
+            </Button>
+          </SheetTrigger>
+          <SidebarSheet />
+        </Sheet>
       </CardContent>
     </Card>
   )
