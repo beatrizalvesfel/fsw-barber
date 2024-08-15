07ad8650aad2efc6a4937cc95f81a2bf22a01354
beatrizalvesfel <beatrizalvesfel@gmail.com>
2024-08-15 16:01:08 -0300

    refactor: ensure decimal warning is not shown on console

--- app/bookings/page.tsx
+++ app/bookings/page.tsx
@@ -1,13 +1,13 @@
 import { getServerSession } from "next-auth"
 import Header from "../_components/header"
+import { db } from "../_lib/prisma"
 import { authOptions } from "../_lib/auth"
 import BookingItem from "../_components/booking-item"
-import { Dialog, DialogContent, DialogTrigger } from "../_components/ui/dialog"
-import SignInDialog from "../_components/sign-in-dialog"
+import { Dialog, DialogTrigger } from "../_components/ui/dialog"
 import { Button } from "../_components/ui/button"
+import { DialogContent } from "@radix-ui/react-dialog"
 import { LogInIcon } from "lucide-react"
-import { getConfirmedBookings } from "../_data/get-confirmed-bookings"
-import { getConcludedBookings } from "../_data/get-concluded-bookings"
+import SignInDialog from "../_components/sign-in-dialog"
 
 const Bookings = async () => {
   const session = await getServerSession(authOptions)
@@ -29,17 +29,48 @@ const Bookings = async () => {
       </div>
     )
   }
-  const confirmedBookings = await getConfirmedBookings()
-  const concludedBookings = await getConcludedBookings()
+  const confirmedBookings = await db.booking.findMany({
+    where: {
+      userId: (session.user as any).id,
+      date: {
+        gte: new Date(),
+      },
+    },
+    include: {
+      service: {
+        include: {
+          barbershop: true,
+        },
+      },
+    },
+    orderBy: {
+      date: "asc",
+    },
+  })
+  const concludedBookings = await db.booking.findMany({
+    where: {
+      userId: (session.user as any).id,
+      date: {
+        lt: new Date(),
+      },
+    },
+    include: {
+      service: {
+        include: {
+          barbershop: true,
+        },
+      },
+    },
+    orderBy: {
+      date: "asc",
+    },
+  })
 
   return (
     <>
       <Header />
       <div className="space-y-3 p-5">
         <h1 className="text-xl font-bold">Agendamentos</h1>
-        {confirmedBookings.length === 0 && concludedBookings.length === 0 && (
-          <p className="text-gray-400">Você não tem agendamentos.</p>
-        )}
         {confirmedBookings.length > 0 && (
           <>
             <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
