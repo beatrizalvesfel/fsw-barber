327e3f4178466831abfe1b06d6e1f202a2b4be72
beatrizalvesfel <beatrizalvesfel@gmail.com>
2024-08-15 16:02:35 -0300

    refactor: add data functions to get bookings

--- app/bookings/page.tsx
+++ app/bookings/page.tsx
@@ -1,6 +1,5 @@
 import { getServerSession } from "next-auth"
 import Header from "../_components/header"
-import { db } from "../_lib/prisma"
 import { authOptions } from "../_lib/auth"
 import BookingItem from "../_components/booking-item"
 import { Dialog, DialogTrigger } from "../_components/ui/dialog"
@@ -8,6 +7,8 @@ import { Button } from "../_components/ui/button"
 import { DialogContent } from "@radix-ui/react-dialog"
 import { LogInIcon } from "lucide-react"
 import SignInDialog from "../_components/sign-in-dialog"
+import { getConfirmedBookings } from "../_data/get-confirmed-bookings"
+import { getConcludedBookings } from "../_data/get-concluded-bookings"
 
 const Bookings = async () => {
   const session = await getServerSession(authOptions)
@@ -29,48 +30,17 @@ const Bookings = async () => {
       </div>
     )
   }
-  const confirmedBookings = await db.booking.findMany({
-    where: {
-      userId: (session.user as any).id,
-      date: {
-        gte: new Date(),
-      },
-    },
-    include: {
-      service: {
-        include: {
-          barbershop: true,
-        },
-      },
-    },
-    orderBy: {
-      date: "asc",
-    },
-  })
-  const concludedBookings = await db.booking.findMany({
-    where: {
-      userId: (session.user as any).id,
-      date: {
-        lt: new Date(),
-      },
-    },
-    include: {
-      service: {
-        include: {
-          barbershop: true,
-        },
-      },
-    },
-    orderBy: {
-      date: "asc",
-    },
-  })
+  const confirmedBookings = await getConfirmedBookings()
+  const concludedBookings = await getConcludedBookings()
 
   return (
     <>
       <Header />
       <div className="space-y-3 p-5">
         <h1 className="text-xl font-bold">Agendamentos</h1>
+        {confirmedBookings.length === 0 && concludedBookings.length === 0 && (
+          <p className="text-gray-400">Você não tem agendamentos.</p>
+        )}
         {confirmedBookings.length > 0 && (
           <>
             <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
